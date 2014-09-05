mnemonyc/platform_packages_apps_dialer_caf

Add call log search in dialer
- add the support to search call log by name and number in
  dialer for dsds and ssss

Change-Id: I963be0a96e06bf36ba47d042987144b25e8af7d4

src/com/android/dialer/calllog/CallLogAdapter.java
@@ -105,6 +105,8 @@ public class CallLogAdapter extends GroupingListAdapter
     private final CallFetcher mCallFetcher;
     private ViewTreeObserver mViewTreeObserver = null;
 
+    private String mFilterString;
+
     /**
      * A cache of the contact details for the phone numbers in the call log.
      * <p>
@@ -676,7 +678,7 @@ public class CallLogAdapter extends GroupingListAdapter
         // New items also use the highlighted version of the text.
         final boolean isHighlighted = isNew;
         mCallLogViewsHelper.setPhoneCallDetails(views, details, isHighlighted,
-                mUseCallAsPrimaryAction);
+                mUseCallAsPrimaryAction, mFilterString);
 
         if (photoId == 0 && photoUri != null) {
             setPhoto(views, photoUri, lookupUri);
@@ -982,5 +984,9 @@ public class CallLogAdapter extends GroupingListAdapter
             number = matchingNumber;
         }
         return number;
+    }
+
+    public void setQueryString(String filter) {
+        mFilterString = filter;
     }
 }

src/com/android/dialer/calllog/CallLogListItemHelper.java
@@ -59,8 +59,14 @@ import com.android.dialer.R;
      */
     public void setPhoneCallDetails(CallLogListItemViews views, PhoneCallDetails details,
             boolean isHighlighted, boolean useCallAsPrimaryAction) {
+        setPhoneCallDetails(views, details, isHighlighted,
+                useCallAsPrimaryAction, null);
+    }
+
+    public void setPhoneCallDetails(CallLogListItemViews views, PhoneCallDetails details,
+            boolean isHighlighted, boolean useCallAsPrimaryAction, String filter) {
         mPhoneCallDetailsHelper.setPhoneCallDetails(views.phoneCallDetailsViews, details,
-                isHighlighted);
+                isHighlighted, filter);
         boolean canCall = PhoneNumberUtilsWrapper.canPlaceCallsTo(details.number,
                 details.numberPresentation);
         boolean canPlay = details.callTypes[0] == Calls.VOICEMAIL_TYPE;

PAC-ROM/android_packages_apps_Dialer

Conflicts:
	src/com/android/dialer/calllog/CallLogActivity.java
	src/com/android/dialer/calllog/CallLogAdapter.java
	src/com/android/dialer/calllog/CallLogListItemHelper.java
	tests/src/com/android/dialer/PhoneCallDetailsHelperTest.java

src/com/android/dialer/calllog/CallLogAdapter.java
@@ -95,6 +95,8 @@ public class CallLogAdapter extends GroupingListAdapter
 
     private String mStatsLabel = null;
 
+    private String mFilterString;
+
     /** Listener for the primary or secondary actions in the list.
      *  Primary opens the call details.
      *  Secondary calls or plays.
@@ -319,7 +321,7 @@ public class CallLogAdapter extends GroupingListAdapter
         // New items also use the highlighted version of the text.
         final boolean isHighlighted = isNew;
         mCallLogViewsHelper.setPhoneCallDetails(views, details, isHighlighted,
-                mShowSecondaryActionButton);
+                mShowSecondaryActionButton, mFilterString);
 
         int contactType = ContactPhotoManager.TYPE_DEFAULT;
 
@@ -628,5 +630,9 @@ public class CallLogAdapter extends GroupingListAdapter
 
     public String getStatsLabel() {
         return mStatsLabel;
+    }
+
+    public void setQueryString(String filter) {
+        mFilterString = filter;
     }
 }

src/com/android/dialer/calllog/CallLogListItemHelper.java
@@ -50,6 +50,12 @@ import com.android.dialer.R;
         mResources = resources;
     }
 
+    public void setPhoneCallDetails(CallLogListItemViews views, PhoneCallDetails details,
+            boolean isHighlighted, boolean showSecondaryActionButton) {
+        setPhoneCallDetails(views, details, isHighlighted,
+            showSecondaryActionButton, null);
+    }
+
     /**
      * Sets the name, label, and number for a contact.
      *
@@ -59,9 +65,9 @@ import com.android.dialer.R;
      * @param showSecondaryActionButton whether to show the secondary action button or not
      */
     public void setPhoneCallDetails(CallLogListItemViews views, PhoneCallDetails details,
-            boolean isHighlighted, boolean showSecondaryActionButton) {
+            boolean isHighlighted, boolean showSecondaryActionButton, String filter) {
         mPhoneCallDetailsHelper.setPhoneCallDetails(views.phoneCallDetailsViews, details,
-                isHighlighted);
+                isHighlighted, filter);
         boolean canPlay = details.callTypes[0] == Calls.VOICEMAIL_TYPE;
 
         // Set the accessibility text for the contact badge